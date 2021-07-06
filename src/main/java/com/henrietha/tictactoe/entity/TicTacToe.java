6a07e9da5e2a4585721ac980708965595ac61ce8
Henrietha <areathy33@gmail.com>
2021-07-06 12:01:47 +0100

    More method for game logic

--- src/main/java/com/henrietha/tictactoe/entity/TicTacToe.java
+++ src/main/java/com/henrietha/tictactoe/entity/TicTacToe.java
@@ -74,7 +74,71 @@ public class TicTacToe {
         System.out.println();
     }
 
-//    public boolean isThereAWinner() {
-//
-//    }
+    public boolean isThereAWinner() {
+        boolean diagonalsAndMidles = rightDi() || leftDi() || middleRow() || secondCol() && board[4] != '-';
+        boolean topAndFirst = topRow() || firstCol() && board[0] != '-';
+        boolean bottomAndThird = bottomRow() || thirdCol() && board[8] != '-';
+
+        if(diagonalsAndMidles) {
+            this.winner = board[4];
+        } else if ( topAndFirst) {
+            this.winner = board[0];
+        } else if (bottomAndThird) {
+            this.winner = board[8];
+        }
+        return diagonalsAndMidles || topAndFirst || bottomAndThird;
+    }
+
+    public boolean topRow() {
+        return board[0] == board[1] && board[1] == board[2];
+    }
+
+    public boolean middleRow() {
+        return board[3] == board[4] && board[4] == board[5];
+    }
+
+    public boolean bottomRow() {
+        return board[6] == board[7] && board[7] == board[8];
+    }
+
+    public boolean firstCol() {
+        return board[0] == board[3] && board[3] == board[6];
+    }
+
+    public boolean secondCol() {
+        return board[1] == board[4] && board[4] == board[7];
+    }
+
+    public boolean thirdCol() {
+        return board[2] == board[5] && board[5] == board[8];
+    }
+
+    public boolean rightDi() {
+        return board[0] == board[4] && board[4] == board[8];
+    }
+
+    public boolean leftDi() {
+        return board[2] == board[4] && board[4] == board[6];
+    }
+
+
+    public boolean isTheBoardFilled() {
+        for(int i=0; i<board.length; i++) {
+            if (board[i] == '-') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String gameOver() {
+        boolean didSomeOneWin = isThereAWinner();
+        if (didSomeOneWin) {
+            return "The winner is" + this.winner + "'s";
+        } else if (isTheBoardFilled()) {
+            return "Draw";
+        } else {
+            return "Keep playing.";
+        }
+    }
 }
