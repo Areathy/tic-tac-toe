1a4d55ba7a5a25a993e028a226a0fd81c1ae06a6
Henrietha <areathy33@gmail.com>
2021-07-06 11:23:39 +0100

    Added some methods

--- src/main/java/com/henrietha/tictactoe/entity/TicTacToe.java
+++ src/main/java/com/henrietha/tictactoe/entity/TicTacToe.java
@@ -21,4 +21,60 @@ public class TicTacToe {
         }
         return board;
     }
+
+    public boolean playTurn(int spot) {
+        boolean isValid = withinRange(spot) && !isSpotTaken(spot);
+        if(isValid) {
+            board[spot-1] = currentMaker;
+            currentMaker = (currentMaker == userMarker) ? aiMarker : userMarker;
+//            return isValid;
+        }
+        return isValid;
+//        return false;
+//        System.out.println("Try again");
+    }
+
+    public boolean withinRange(int number) {
+        return number > 0 && number < board.length + 1;
+    }
+
+    public boolean isSpotTaken(int number) {
+        return board[number-1] != '-';
+    }
+
+    public void printBoard() {
+        // Attempting to create...
+        // | - | - | -
+        // ------------
+        // | - | - | -
+        // ------------
+        // | - | - | -
+
+        System.out.println();
+        for (int i = 0; i<board.length; i++) {
+            if(i % 3 == 0 && i !=0) {
+                System.out.println();
+                System.out.println("------------");
+            }
+            System.out.println(" | " + board[1]);
+        }
+        System.out.println();
+    }
+
+    public void printIndexBoard() {
+
+        System.out.println();
+        for (int i = 0; i<board.length; i++) {
+            if(i % 3 == 0 && i !=0) {
+                System.out.println();
+                System.out.println("------------");
+            }
+            System.out.println(" | " + (i +1));
+        }
+        System.out.println();
+    }
+
+//    public boolean isThereAWinner() {
+//
+//    }
 }
